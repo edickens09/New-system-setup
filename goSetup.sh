29eb1702c04bad7e4f74f2cd315f15e2dacd9dfa
edickens09 <edickens09@gmail.com>
2025-08-22 06:51:46 -0400

    creating braches so that can be used to also update packages

--- goSetup.sh
+++ goSetup.sh
@@ -1,8 +1,9 @@
 #Install go using webi
 
-curl -sS https://webi.sh/golang | sh
-source ~/.config/envman/PATH.env
-
+if ! command -v go; then 
+	curl -sS https://webi.sh/golang | sh;\
+	source ~/.config/envman/PATH.env
+fi
 #change cgo to use zig
 go env -w CC="zig cc"
 go env -w CXX="zig c++"
