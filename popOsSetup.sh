c1af64af5f1dfe9dc444b3d4c769515ae6bb7489
Eric Dickens <eric@neovivify.com>
2026-01-14 09:28:50 -0500

    updating spacing for easier compatibilityy

--- popOsSetup.sh
+++ popOsSetup.sh
@@ -28,62 +28,62 @@ source ~/.bashrc
 curl -sSfL https://raw.githubusercontent.com/edickens09/New-system-setup/main/snapRemove.sh -o "$tempDir/snapRemove.sh"
 
 if [ -s "$tempDir/snapRemove.sh" ]; then
-	source "$tempDir/snapRemove.sh"
+    source "$tempDir/snapRemove.sh"
 else
-	echo "Download failed"
+    echo "Download failed"
 fi
 
 #Install Go
 curl -sSfL https://raw.githubusercontent.com/edickens09/New-system-setup/main/goSetup.sh -o "$tempDir/goSetup.sh"
 
 if [ -s "$tempDir/goSetup.sh" ]; then
-	source "$tempDir/goSetup.sh"
+    source "$tempDir/goSetup.sh"
 else
-	echo "Download failed"
+    echo "Download failed"
 fi
 
 # install and setup docker
 curl -sSfL https://raw.githubusercontent.com/edickens09/New-system-setup/main/dockerInstallPop.sh -o "$tempDir/dockerInstall.sh"
 if [ -s "$tempDir/dockerInstall.sh" ]; then
-	source "$tempDir/dockerInstall.sh"
+    source "$tempDir/dockerInstall.sh"
 else
-	echo "Download failed"
+    echo "Download failed"
 fi
 
 # install and setup git
 curl -sSfL https://raw.githubusercontent.com/edickens09/New-system-setup/main/gitSetup.sh -o "$tempDir/gitSetup.sh"
 
 if [ -s "$tempDir/gitSetup.sh" ]; then
-	source "$tempDir/gitSetup.sh"
+    source "$tempDir/gitSetup.sh"
 else
-	echo "Download failed"
+    echo "Download failed"
 fi
 
 # Install flathub and flatpak apps
 curl -sSfL https://raw.githubusercontent.com/edickens09/New-system-setup/main/flatpak.sh -o "$tempDir/flatpak.sh"
 
 if [ -s "$tempDir/flatpak.sh" ]; then
-	source "$tempDir/flatpak.sh"
+    source "$tempDir/flatpak.sh"
 else
-	echo "Download failed flatpak"
+    echo "Download failed flatpak"
 fi
 
 #Install neovim from github release
 curl -sSfL https://raw.githubusercontent.com/edickens09/New-system-setup/main/nvim.sh -o "$tempDir/nvim.sh"
 
 if [ -s "$tempDir/nvim.sh" ]; then
-	source "$tempDir/nvim.sh"
+    source "$tempDir/nvim.sh"
 else
-	echo "Download failed nvim"
+    echo "Download failed nvim"
 fi
 
 #Install 1password
 curl -sSfL https://raw.githubusercontent.com/edickens09/New-system-setup/main/1password.sh -o "$tempDir/1password.sh"
 
 if [ -s "$tempDir/1password.sh" ]; then
-	source "$tempDir/1password.sh"
+    source "$tempDir/1password.sh"
 else
-	echo "Download failed 1password"
+    echo "Download failed 1password"
 fi
 
 #Setup development filestructure
