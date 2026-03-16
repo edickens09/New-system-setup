2a2f2e732a2964d65504700ec73407ba1afe1d2c
Eric <eric@neovivify.com>
2026-03-15 21:47:25 -0400

    consolidating dockerInstall to one file

--- dockerInstall.sh
+++ dockerInstall.sh
@@ -9,11 +9,15 @@ if command -v apt >/dev/null 2>&1; then
     sudo apt-get update
     sudo apt-get install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
+    #debian install needs split install
+    #sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources:
     echo \
+        #debian install needs split install
+        #"deb [arch=$(dpkg --pring-architecture) signed-by=etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \"
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
